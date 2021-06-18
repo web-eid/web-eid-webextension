273bf504775b652c241a9defd5fccf9f89675c69
Mart Somermaa <mrts@users.noreply.github.com>
2021-06-18 23:35:19 +0300

    feat(error-handling): add ERR_WEBEID_NATIVE_INVALID_ARGUMENT error, bump version to 1.0.0
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- src/background/actions/TokenSigning/errorToResponse.ts
+++ src/background/actions/TokenSigning/errorToResponse.ts
@@ -28,7 +28,8 @@ import tokenSigningResponse from "../../../shared/tokenSigningResponse";
 export default function errorToResponse(nonce: string, error: any): TokenSigningErrorResponse {
   if (error.code === ErrorCode.ERR_WEBEID_USER_CANCELLED) {
     return tokenSigningResponse<TokenSigningErrorResponse>("user_cancel", nonce);
-  } else if (error.code === ErrorCode.ERR_WEBEID_NATIVE_FATAL) {
+  } else if (error.code === ErrorCode.ERR_WEBEID_NATIVE_FATAL ||
+             error.code === ErrorCode.ERR_WEBEID_NATIVE_INVALID_ARGUMENT) {
     const nativeException = serializeError(error);
 
     return tokenSigningResponse<TokenSigningErrorResponse>("driver_error", nonce, { nativeException });
