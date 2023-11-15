4b1afcd3634f49a1f2100ac803ee3124797ff47c
Raul Metsma <raul@metsma.ee>
2023-11-15 14:08:40 +0200

    Polyfill is still needed in chrome
    WE2-789
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import alias from "@rollup/plugin-alias";
 import cleanup from "rollup-plugin-cleanup";
 import injectProcessEnv from "rollup-plugin-inject-process-env";
 import license from "rollup-plugin-license";
+import polyfill from "rollup-plugin-polyfill";
 import resolve from "@rollup/plugin-node-resolve";
 
 const projectRootDir = path.resolve(__dirname);
@@ -11,8 +12,8 @@ const projectRootDir = path.resolve(__dirname);
 const pluginsConf = [
   alias({
     entries: [{
-      find: "@web-eid.js",
-      replacement: path.resolve(projectRootDir, "dist/lib/web-eid.js/src")
+      find:        "@web-eid.js",
+      replacement: path.resolve(projectRootDir, "dist/lib/web-eid.js/src"),
     }],
   }),
   resolve({ rootDir: "./dist" }),
@@ -24,7 +25,7 @@ const pluginsConf = [
   license({
     banner: {
       content: {
-        file: path.join(projectRootDir, "LICENSE"),
+        file:     path.join(projectRootDir, "LICENSE"),
         encoding: "utf-8",
       },
     },
@@ -37,24 +38,24 @@ const browsers = ["chrome", "firefox", "safari"];
 // Use flatMap() to create a configuration for each browser and each of the "content" and "background" scripts.
 const browserConfigs = browsers.flatMap((browser) =>
   ["content", "background"].map((name) => ({
-    input: `./dist/${browser}/${name}/${name}.js`,
+    input:  `./dist/${browser}/${name}/${name}.js`,
     output: [
       {
-        file: `dist/${browser}/${name}.js`,
-        format: "iife",
+        file:      `dist/${browser}/${name}.js`,
+        format:    "iife",
         sourcemap: name === "background",
       },
     ],
-    plugins: pluginsConf,
+    plugins: [...pluginsConf, polyfill(["webextension-polyfill"])],
     context: "window",
   }))
 );
 
 // Define the configuration for the TokenSigning compatibility page script for Chrome and Firefox.
 const tokenSigningPageConfig = {
-  input: "./dist/firefox/resources/token-signing-page-script.js",
+  input:  "./dist/firefox/resources/token-signing-page-script.js",
   output: ["chrome", "firefox"].map((browser) => ({
-    file: `dist/${browser}/token-signing-page-script.js`,
+    file:   `dist/${browser}/token-signing-page-script.js`,
     format: "iife",
   })),
   plugins: pluginsConf,
