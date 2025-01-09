32b32846d9e7711f8daf3f34eab86b93cec4e016
Tanel Metsar <taneltm@users.noreply.github.com>
2025-01-09 12:38:33 +0000

    Updated dependencies and version bump to 2.4.0-rc1
    WE2-1018
    
    - All dependencies updated
    - Version bump to 2.4.0-rc1
    - Version in package.json can now have a release candidate suffix which will be excluded in the extension manifest
    - Migrated `eslint` to the new flat config file format
    - The `@typescript-eslint/eslint-plugin` and `@typescript-eslint/parser` packages are deprecated, changed to the new `typescript-eslint` package which contains both.
    - New dependency `@stylistic/eslint-plugin-ts` - Some linter rules have been deprecated or removed from the `typescript-eslint` project and migrated to `@stylistic`, for example the `semi` rule.
    - Fixed some minor linter errors
    - Added additional linter rule overrides. These rules should be checked separately and might require fixes in the code.
    - Renamed `rollup.config.js` to `rollup.config.mjs` as it was a ES6 module and not a CommonJS file
    - Removed `findFiles` from `build-utils.mjs`. It was a promise wrapper for `glob`. After updating `glob`, it now returns a promise by itself, so the wrapper was unnecessary.
    - Moved `bootstrap` from `devDependencies` to `dependencies`, which should now correctly reflect how it's used.
    
    Signed-off-by: Tanel Metsar <taneltm@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { pathsToModuleNameMapper } = require("ts-jest");
 const { compilerOptions } = require("./tsconfig");
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset:           "ts-jest",
   testEnvironment:  "node",
